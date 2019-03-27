ea7ec7d1a8231a82533b67ad70c2dc40090ec496
github username <github email address>
2019-03-27 12:43:23 -0400

    done

---
+++ 10001st-prime/index.js
@@ -0,0 +1,20 @@
+function isPrime(num) {
+  for(let i = 2; i < num; i++) {
+    if(num % i === 0) {
+      return false
+    }
+  }
+  return num
+}
+
+function findPrime(number) {
+  let primeArray = [2]
+  // primeArray.length < number b/c we need an array with length of 10,000 b/c that would be the 10,001st prime
+  // i+=2 b/c no even numbers are prime numbers
+  for(let i = 3; primeArray.length < number; i+=2) {
+    if (isPrime(i)) {
+      primeArray.push(isPrime(i))
+    }
+  }
+  return primeArray[primeArray.length - 1]
+}
