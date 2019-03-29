f1b9348b2595136cd5850098eca866006107ef19
github username <josephspinelli@yahoo.com>
2019-03-29 14:51:54 -0400

    done

--- smallest-multiple/index.js
+++ smallest-multiple/index.js
@@ -1,13 +1,13 @@
+// 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+// What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
 function smallestMultiple(){
-
   let i = 2520; // smallest 1-10 divisible numbers
-
   let found = false;
 
   while (found==false){
     i+= 2520;
     let isDivisible = true;
-
     //b/c 11-20 is divisible by same numbers as 1-10
     for(let j=11; j<=20; j++){
       console.log(i, j)
