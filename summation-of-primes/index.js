7bf98266efabdf00d972b49815cadcfc2f0cccb5
github username <josephspinelli@yahoo.com>
2019-04-05 15:06:03 -0400

    done

--- summation-of-primes/index.js
+++ summation-of-primes/index.js
@@ -1,3 +1,6 @@
+// The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+// Find the sum of all the primes below two million.
+
 function isPrime(num) {
   for(let i = 2; i < num; i++) {
     if(num % i === 0) {
