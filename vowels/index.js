1d53cb453c4c624cc73a34cc23d17acb22df2ca6
github username <josephspinelli@yahoo.com>
2019-04-17 15:12:17 -0400

    done

---
+++ vowels/index.js
@@ -0,0 +1,16 @@
+// Write a function that returns the number of vowels used in a string.  Vowels are the characters
+// 'a', 'e', 'i', 'o', and 'u'.
+// Examples:
+//   vowels('Hi There!') --> 3
+//   vowels('Why do you ask?') --> 4
+//   vowels('Why?') --> 0
+
+function vowels(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  let vowelArray = [];
+
+  str.toLowerCase().split('').map( letter => {
+    (vowels.includes(letter) ? vowelArray.push(letter) : null)
+  })
+  return vowelArray.length
+}
