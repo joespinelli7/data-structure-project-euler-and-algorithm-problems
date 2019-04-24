1fc21da5aeba57d36635e747ff678466e271157d
github username <josephspinelli@yahoo.com>
2019-04-24 11:38:12 -0400

    done

--- matrix/index.js
+++ matrix/index.js
@@ -14,5 +14,53 @@
 //     [10,  9,  8, 7]]
 
 function matrix(n) {
-  
+  //create an empty array of arrays
+  const results = [];
+
+  for (let i = 0; i < n; i++) {
+    results.push([]);
+  }
+
+  //create counter variable starting at 1
+  let counter = 1;
+  //create variables for columns and rows of array
+  let startColumn = 0;
+  let endColumn = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
+
+  //as long as (start column <= end column) AND (start row <= end row)
+  while (startColumn <= endColumn && startRow <= endRow) {
+    //top row, looping from start column to end column
+    for (let i = startColumn; i <= endColumn; i++) {
+      //inside loop at every step assign some value to results array
+      results[startRow][i] = counter;
+      counter++;
+    }
+    //increment start row to move onto next subarray
+    startRow++;
+
+    //right column, loop front start to end row
+    for (let i = startRow; i <= endRow; i++) {
+      results[i][endColumn] = counter;
+      counter++;
+    }
+    endColumn--;
+
+    //botton row,
+    for (let i = endColumn; i >= startColumn; i--) {
+      results[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+
+    //start column
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = counter;
+      counter++;
+    }
+    startColumn++;
+  }
+
+  return results;
 }
