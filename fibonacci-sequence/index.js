cef317ba063ec97d35089850e9aa24be1c556796
github username <josephspinelli@yahoo.com>
2019-05-21 12:31:28 -0400

    done

--- fibonacci-sequence/index.js
+++ fibonacci-sequence/index.js
@@ -27,3 +27,30 @@ function fib(n) {
 //
 //   return fib(n - 1) + fib(n - 2)
 // }
+
+// Memoization solution:
+// passes a function (fn) into it:
+// ...args takes unknown # of arguments and transforms them into an array
+// function memoize(fn) {
+//   const cache = {};
+//   return function(...args) {
+//     if(cache[args]) {
+//       return cache[args];
+//     }
+//
+//     const result = fn.apply(this, args);
+//     cache[args] = result;
+//
+//     return result;
+//   };
+// }
+//
+// function slowFib(n) {
+//   if (n < 2) {
+//     return n;
+//   }
+//
+//   return fib(n - 1) + fib(n - 2);
+// }
+//
+// const fib = memoize(slowFib);
