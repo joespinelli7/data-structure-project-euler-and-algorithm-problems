a1d9442017beb65cce0c95d32630dda2e6db77b3
github username <josephspinelli@yahoo.com>
2019-07-03 14:29:33 -0400

    done

--- pyramid/index.js
+++ pyramid/index.js
@@ -11,23 +11,44 @@
 //       ' ### '
 //       '#####'
 
+// Improved solution:
 function pyramid(n) {
-  (n <= 0 ? alert("Positive numbers only!") : null);
-  let string = "#";
-  let addToString = "##";
-  let emptySpace = " ";
+  n > 0 ? null : alert("Positive numbers only!");
+  let hash = '#';
+  let space = ' ';
 
-  for(let i = 1; i <= n; i++) {
-    let numberOfSpaces = n - i;
+  for (let i = 1; i <= n; i++) {
+    let numOfSpaces = n - i;
 
-    if (numberOfSpaces > 1 && i === 1) {
-      console.log(emptySpace.repeat(numberOfSpaces) + string.repeat(i) + emptySpace.repeat(numberOfSpaces))
-    } else if (numberOfSpaces > 1 && i > 1) {
-      console.log(emptySpace.repeat(numberOfSpaces) + string + addToString.repeat(i - 1) + emptySpace.repeat(numberOfSpaces))
-    } else if (numberOfSpaces === 1) {
-      console.log(emptySpace.repeat(numberOfSpaces) + string + addToString.repeat(n - 2) + emptySpace.repeat(numberOfSpaces))
+    if (numOfSpaces === n - 1) {
+      console.log(space.repeat(numOfSpaces) + hash.repeat(i) + space.repeat(numOfSpaces));
+    } else if (numOfSpaces > 0 ) {
+      console.log(space.repeat(numOfSpaces) + hash.repeat((i * 2) - 1) + space.repeat(numOfSpaces));
     } else {
-      console.log(string + addToString.repeat(n - 1))
+      console.log(hash.repeat((n * 2) - 1));
     }
   }
+
 }
+
+// Old solution:
+// function pyramid(n) {
+//   (n <= 0 ? alert("Positive numbers only!") : null);
+//   let string = "#";
+//   let addToString = "##";
+//   let emptySpace = " ";
+//
+//   for(let i = 1; i <= n; i++) {
+//     let numberOfSpaces = n - i;
+//
+//     if (numberOfSpaces > 1 && i === 1) {
+//       console.log(emptySpace.repeat(numberOfSpaces) + string.repeat(i) + emptySpace.repeat(numberOfSpaces))
+//     } else if (numberOfSpaces > 1 && i > 1) {
+//       console.log(emptySpace.repeat(numberOfSpaces) + string + addToString.repeat(i - 1) + emptySpace.repeat(numberOfSpaces))
+//     } else if (numberOfSpaces === 1) {
+//       console.log(emptySpace.repeat(numberOfSpaces) + string + addToString.repeat(n - 2) + emptySpace.repeat(numberOfSpaces))
+//     } else {
+//       console.log(string + addToString.repeat(n - 1))
+//     }
+//   }
+// }
