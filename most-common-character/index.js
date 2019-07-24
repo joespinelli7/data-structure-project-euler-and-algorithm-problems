b08d252ccec32b7430a63b9bbfed435bfa4ab47c
github username <josephspinelli@yahoo.com>
2019-07-24 14:06:47 -0400

    done

--- most-common-character/index.js
+++ most-common-character/index.js
@@ -20,3 +20,15 @@ function maxChar(str) {
   }
   return maxC;
 }
+
+// new solution: (7 less lines of code)
+// function maxChar(str) {
+//   let charHolder = {};
+//
+//   for (let char of str) {
+//     (charHolder[char] ? charHolder[char]++ : charHolder[char] = 1);
+//   }
+//
+//   let highestValue = Object.values(charHolder).sort((a, b) => {return b-a})[0];
+//   return Object.keys(charHolder).find(char => charHolder[char] === highestValue);
+// }
