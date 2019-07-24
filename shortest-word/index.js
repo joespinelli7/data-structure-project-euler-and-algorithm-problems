1362feb39ebb9b3e0d3b3ee75d3fbb1891cfe113
github username <josephspinelli@yahoo.com>
2019-07-24 14:42:53 -0400

    done

--- shortest-word/index.js
+++ shortest-word/index.js
@@ -2,7 +2,7 @@
 // String will never be empty and you do not need to account for different data types.
 
 function findShort(str){
-   return str.split(' ').map(word => {
+  return str.split(' ').map(word => {
     return word.length
   })
   .sort((a,b) => {return a - b})[0];
