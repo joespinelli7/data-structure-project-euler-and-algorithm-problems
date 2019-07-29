e9114f1b59d4ce41ae01a1da64caa10e0c8e3074
github username <josephspinelli@yahoo.com>
2019-07-28 21:26:34 -0400

    done

--- queue/index.js
+++ queue/index.js
@@ -1,4 +1,3 @@
-// --- Description
 // Create a queue data structure.  The queue should be a class with methods 'add' and 'remove'.
 // Adding to the queue should store an element until it is removed.
 // Examples:
