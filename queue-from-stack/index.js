62c2e8a1e82b1ca060094e8feba4baa6c5a58deb
github username <josephspinelli@yahoo.com>
2019-08-01 00:12:42 -0400

    done

--- queue-from-stack/index.js
+++ queue-from-stack/index.js
@@ -28,5 +28,38 @@ class Stack {
 }
 
 class Queue {
+  constructor() {
+    this.firstStack = new Stack();
+    this.secondStack = new Stack();
+  }
 
+  add(record) {
+    this.firstStack.push(record);
+  }
+
+  remove() {
+    while (this.firstStack.peek()) {
+      this.secondStack.push(this.firstStack.pop());
+    }
+
+    const record = this.secondStack.pop();
+    while (this.secondStack.peek()) {
+      this.firstStack.push(this.secondStack.pop());
+    }
+
+    return record;
+  }
+
+  peek() {
+    while (this.firstStack.peek()) {
+      this.secondStack.push(this.firstStack.pop());
+    }
+
+    const record = this.secondStack.peek();
+    while (this.secondStack.peek()) {
+      this.firstStack.push(this.secondStack.pop());
+    }
+
+    return record;
+  }
 }
