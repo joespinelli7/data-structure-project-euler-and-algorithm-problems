141fb0070f238eaecf6a1953916b35b1e9687d67
github username <josephspinelli@yahoo.com>
2019-09-06 13:03:12 -0500

    circular set up

---
+++ circular/index.js
@@ -0,0 +1,18 @@
+// Given a linked list, return true if the list is circular, false if it is not.
+// A circular linked list, is a list that does not have a tail node. Instead,
+// last node points to an earlier node within the list.
+// This is a problem and linked lists should not be circular.
+// Examples:
+//   const l = new List();
+//   const a = new Node('a');
+//   const b = new Node('b');
+//   const c = new Node('c');
+//   l.head = a;
+//   a.next = b;
+//   b.next = c;
+//   c.next = b;
+//   circular(l) // true
+
+function circular(list) {
+
+}
