e5b2a24889d32b636397bec422deb9dbbf10a2bb
github username <josephspinelli@yahoo.com>
2019-09-06 13:21:30 -0500

    done

--- circular/index.js
+++ circular/index.js
@@ -14,5 +14,24 @@
 //   circular(l) // true
 
 function circular(list) {
+  // slow will be moving 1 node per iteration and fast will be moving 2 nodes per iteration
+  let slow = list.head; // or list.getFirst();
+  let fast = list.head;
 
+  // first checks if there is at least a next node in the list (in case uneven amount of nodes in list).
+  // AND(&&) checks while the list has at least 2 more nodes inside it, move slow by 1 node and fast by 2 nodes.
+  // IF at any point slow === false, we know that the final node is pointing to another node in the
+  // list and we are dealing with a circular list, so return true.
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    // we are comparing if they are the exact same instance node.
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  // otherwise, if either fast.next or fast.next.next is false(null), signifies there must be an end
+  // to list, so we break out of the while loop and return false.
+  return false;
 }
