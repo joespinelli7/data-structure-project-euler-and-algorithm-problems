6c64c71e3ae2e14798762ccc91a962a7c8f03658
github username <josephspinelli@yahoo.com>
2019-09-08 22:04:00 -0500

    comments

--- from-last/index.js
+++ from-last/index.js
@@ -1,4 +1,4 @@
-// Given a linked list, return the element n spaces from the last node in the list.  Do not call the
+// Given a linked list, return the element n spaces from the last node in the list. Do not call the
 // 'size' method of the linked list. Assume that n will always be less than the length of the list.
 // Examples:
 //    const list = new List();
