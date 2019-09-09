d51c5404a90c56cf186a1664e15e810d1a673167
github username <josephspinelli@yahoo.com>
2019-09-09 15:16:21 -0500

    node implemented successfully

--- tree/index.js
+++ tree/index.js
@@ -6,7 +6,20 @@
 // function that gets called with each element in the tree
 
 class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
 
+  add(data) {
+    this.children.push(new Node(data));
+  }
+
+  remove(data) {
+    this.children = this.children.filter(node => {
+      return node.data !== data
+    });
+  }
 }
 
 class Tree {
