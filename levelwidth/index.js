aa13ea226de972c3c4ae6fee5d8f9ec975930252
github username <josephspinelli@yahoo.com>
2019-09-15 16:13:10 -0500

    levelwidth set up

---
+++ levelwidth/index.js
@@ -0,0 +1,14 @@
+// Given the root node of a tree, return an array where each element is the width of the tree at each level.
+// Returns number of nodes in each level of tree.
+// Example:
+// Given:
+//     0
+//   / |  \
+// 1   2   3
+// |       |
+// 4       5
+// Answer: [1, 3, 2]
+
+function levelWidth(root) {
+
+}
