ec6fdc72c7327f28308d240a2822d32e3d430122
github username <josephspinelli@yahoo.com>
2019-09-16 20:38:09 -0500

    insert method complete

--- binary-search-tree/index.js
+++ binary-search-tree/index.js
@@ -12,7 +12,20 @@ class Node {
     this.right = null;
   }
 
+  // recurisve solution:
   insert(data) {
-
+    if (data < this.data && this.left) {
+      // recursion here if data is less than this.data and this.left is already assigned,
+      // we want to run the whole method again except with this.left as node we're focusing on.
+      this.left.insert(data);
+    } else if (data < this.data) {
+      // Else If, then we want to create a new node on the left side b/c there is no current left node.
+      this.left = new Node(data);
+    } else if (data > this.data && this.right) {
+      // same logic here except with right hand side node.
+      this.right.insert(data);
+    } else if (data > this.data) {
+      this.right = new Node(data);
+    }
   }
 }
