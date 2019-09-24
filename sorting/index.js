d162f572c254d46794480bf29201d85aa43143e8
github username <josephspinelli@yahoo.com>
2019-09-23 19:06:11 -0500

    selectionSort complete

--- sorting/index.js
+++ sorting/index.js
@@ -17,7 +17,27 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    // here, saying we believe i is lowest index in array so if not prove me wrong in the following code.
+    let indexOfMin = i;
 
+    for (let j = i + 1; j < arr.length; j++) {
+      // checks IF array at index of j is less than our previously assumed lowest index, update indexOfMin
+      // to that j value (b/c its the new lowest).
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+
+    if (indexOfMin !== i) {
+      // here, arr[indexOfMin] points to the absolute lowest value.
+      let lesser = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+
+  return arr;
 }
 
 function mergeSort(arr) {
